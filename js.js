3ded253c0caba9d6da4d56de87b9bb3ce68269e7
jsdevsl <73017635+jsdevsl@users.noreply.github.com>
2020-10-25 00:22:02 +0800

    Update js.js
    r

--- js.js
+++ js.js
@@ -1,19 +1,3 @@
  
-GroupType=[
-{值：“ 0”，标签：“福利111”，类型：“普通”，selfBuilt：true，comp：“ Fuli”}，
-{值：'1'，标签：'扫雷'，类型：'炸弹'，selfBuilt：true，comp：'Saolei'}，
-{值：'2'，标签：'牛牛'，类型：'niuniu'，selfBuilt：true，comp：'Niuniu'}，
-{值：'3'，标签：'禁抢'，类型：'jinqiang'，selfBuilt：false，comp：'Jinqiang'}，
-{值：'4'，标签：'抢庄牛牛'，类型：'robniuniu'，selfBuilt：true，comp：'Robniu'}，
-{值：“ 5”，标签：“二八杠”，类型：“ erbagang”，selfBuilt：true，comp：“ Robniu”}，
-{值：“ 6”，标签：“龙虎斗”，类型：“ longhd”，selfBuilt：false，comp：“ Longhd”}，
-{值：“ 7”，标签：“接龙”，类型：“ jielong”，selfBuilt：false，comp：“ Jielong”}，
-{值：“ 8”，标签：“二人牛牛”，类型：“ twoniuniu”，selfBuilt：true，comp：“ Niuniu”}，
-{值：'9'，标签：'超级扫雷'，类型：'superBomb'，selfBuilt：false，comp：'SuperBomb'}，
-{值：“ 10”，标签：“包包彩”，类型：“ baoBaoCai”，selfBuilt：false，comp：“ baoBaoCai”}，
-{值：“ 11”，标签：“包包牛”，类型：“ NiuNiuBao”，selfBuilt：false，comp：“ niuNiuBao”}，
-// {值：'12'，标签：'百人牛牛'，类型：'NiuNiuBao'，selfBuilt：false，comp：'niuNiuBao'}，
-// {值：'13'，标签：'极速扫雷'，类型：'NiuNiuBao'，selfBuilt：false，comp：'niuNiuBao'}，
-{值：'14'，标签：'百人牛牛'，类型：'bairenniuniu'，selfBuilt：false，comp：'bairenniuniu'}，
-{值：'15'，标签：'极速扫雷'，类型：'jisusaolei'，selfBuilt：false，comp：'jisusaolei'}，
-];err,gameTypes=null;
+GroupType=[];
+err,gameTypes=null;
